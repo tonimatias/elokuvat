4244cbbf8afefbdc7b836f4584803b1b59009482
tonimatias <tonimatias96@gmail.com>
2022-03-30 10:29:02 +0300

    taulujen luonti

--- sql_scripts/database.sql
+++ sql_scripts/database.sql
@@ -3,20 +3,21 @@ create database elokuvat;
 use elokuvat;
 create Table elokuva (
     /* id int PRIMARY KEY AUTO_INCREMENT, */
-    elokuva_nimi VARCHAR(100) not null unique PRIMARY KEY,
-    tyylilaji_ID int AUTO_INCREMENT,
-    ohjaaja_ID INT
-    julkaisuvuosi INT
+    elokuva_ID INT AUTO_INCREMENT PRIMARY KEY,
+    elokuva_nimi VARCHAR(40) not null,
+    genre_ID int FOREIGN KEY REFERENCES genre(genre_ID),
+    ohjaaja_ID INT FOREIGN KEY REFERENCES ohjaaja(ohjaaja_ID,)
+    julkaisuvuosi DATE NOT NULL
 );
 create Table genre (
-    tyylilaji_ID INT AUTO_INCREMENT,
-    tyylilaji VARCHAR(50),
-    ikaraja INT
-    FOREIGN key 
+    genre_ID INT AUTO_INCREMENT PRIMARY KEY,
+    tyylilaji VARCHAR(50)/* ,
+    ikaraja INT */
+   /*  FOREIGN key  */
 );
 
 create Table ohjaaja (
-    ohjaaja_ID INT AUTO_INCREMENT,
-    ohjaaja VARCHAR(100),
-    elokuvat_lkm INT
+    ohjaaja_ID INT AUTO_INCREMENT PRIMARY KEY,
+    ohjaaja_nimi VARCHAR(100) NOT NULL,
+    elokuvat_lkm INT NOT NULL
 );
