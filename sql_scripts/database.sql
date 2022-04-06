44f0399ae315f0e8a6af4985d01ab98a99103c35
tonimatias <tonimatias96@gmail.com>
2022-04-06 13:24:15 +0300

    taulut korjattu

--- sql_scripts/database.sql
+++ sql_scripts/database.sql
@@ -1,25 +1,27 @@
 drop database if exists elokuvat;
 create database elokuvat;
 use elokuvat;
+
+create Table genre (
+    genre_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tyylilaji VARCHAR(50)/* ,
+    ikaraja INT */
+);
+
+create Table ohjaaja (
+    ohjaaja_ID INT NOT null AUTO_INCREMENT PRIMARY KEY,
+    ohjaaja_nimi VARCHAR(100) NOT NULL,
+    elokuvat_lkm INT NOT NULL
+);
+
 create Table elokuva (
     /* id int PRIMARY KEY AUTO_INCREMENT, */
     elokuva_ID INT AUTO_INCREMENT PRIMARY KEY,
     elokuva_nimi VARCHAR(40) not null,
     genre_ID int NOT NULL,
     ohjaaja_ID INT NOT NULL,
-    julkaisuvuosi DATE NOT NULL
+    julkaisuvuosi DATE NOT NULL,
+    FOREIGN KEY (genre_ID) REFERENCES genre(genre_ID),
+    FOREIGN KEY (ohjaaja_ID) REFERENCES ohjaaja(ohjaaja_ID)
     
-);
-create Table genre (
-    genre_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tyylilaji VARCHAR(50),/* ,
-    ikaraja INT */
-   	FOREIGN KEY (genre_ID) REFERENCES elokuva(genre_ID)
-);
-
-create Table ohjaaja (
-    ohjaaja_ID INT AUTO_INCREMENT PRIMARY KEY,
-    ohjaaja_nimi VARCHAR(100) NOT NULL,
-    elokuvat_lkm INT NOT NULL,
-    FOREIGN KEY ohjaaja_ID REFERENCES elokuva(ohjaaja_ID)
 );
