15dca83c7b52299d57f3669bb1bf46cff786ceae
JSKarhu <92786584+JSKarhu@users.noreply.github.com>
2022-04-12 12:26:11 +0300

    elokuvat tyylilajeittain tulostaa nyt järjestyksessä tyylilajin nimen perusteella

--- public/genre_and_movie.php
+++ public/genre_and_movie.php
@@ -4,7 +4,7 @@ include('../src/templates/head.php');
 require('../db.php');
 
 //Tyylilajit ja elokuvat linkitetty, näyttää jokaisen elokuvan tyylilajin ja nimen:
-$sql = "SELECT tyylilaji, elokuva_nimi FROM genre, elokuva WHERE genre.genre_ID = elokuva.genre_ID;";
+$sql = "SELECT tyylilaji, elokuva_nimi FROM genre, elokuva WHERE genre.genre_ID = elokuva.genre_ID ORDER By tyylilaji;";
 
 //Näin saa yhden genren elokuvat erikseen, mutta ei vaikuta hyvältä tavalta:
 //$sql = "SELECT tyylilaji, elokuva_nimi FROM genre, elokuva WHERE genre.genre_ID = elokuva.genre_ID AND elokuva.genre_ID = 1;";
