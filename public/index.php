911ad364ba2ed65f0dd2f0900dfd9d1212de5c85
tonimatias <tonimatias96@gmail.com>
2022-04-27 12:19:07 +0300

    kirjautumalla etunimi ja sukunimi etusivulle

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
     include('../src/templates/head.php');
-    echo '<h1>Tähän jotain paskaa</h1>';
+    ?>
+    <div>
+     <?php 
+    if(isset($_SESSION["username"])){
+        echo "<h1>Tervetuloa ".$_SESSION["fname"]." ".$_SESSION["lname"]."! Miten menee? :)</h1>";
+    }else{
+        echo "<h1>Tervetuloa! Ylhäältä voit selata mieluisia elokuvia ja kirjautumalla sisään voit itsekkin lisätä!</h1>";
+    }
+     ?> <br>
+    </div>
+    <?php
     include('../src/templates/foot.php');
