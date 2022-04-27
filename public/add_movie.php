06eeb06d6387a9ef44c1eca54b1994af75adf4c8
tonimatias <tonimatias96@gmail.com>
2022-04-27 13:50:39 +0300

    toimiva ohjaaja dropdown

--- public/add_movie.php
+++ public/add_movie.php
@@ -7,7 +7,7 @@ require ('../db.php');
     //Jos parametria ei löydy, funktio palauttaa null
 
     $elokuvanimi = filter_input(INPUT_POST,"elokuvanimi");
-    $genre = filter_input(INPUT_POST,"genre_ID");
+    $genre = filter_input(INPUT_POST,"genreID");
     $ohjaaja = filter_input(INPUT_POST,"ohjaajaID");
     $julkaisuvuosi = filter_input(INPUT_POST,"julkaisuvuosi");
     $genreID = filter_input(INPUT_POST, "genreID", FILTER_SANITIZE_NUMBER_INT);
@@ -41,20 +41,48 @@ require ('../db.php');
         }
         echo "</select><br/>";
     }
+
+    $selectedID = isset($ohjaaja) ? $ohjaaja : 0;
+
+    function ohjaajaDropDown(){
+        require ('../db.php');
+
+        $sql = "SELECT x.ohjaaja_ID, x.ohjaaja_nimi 
+        FROM ohjaaja x, ohjaaja y 
+        WHERE x.ohjaaja_ID = y.ohjaaja_ID ORDER By ohjaaja_ID;";
+
+        $ohjaajat = $pdo->query($sql);
+
+        echo '<select name="ohjaajaID" id="ohjaajaID">';
+            // Loop till there are no more rows
+        foreach($ohjaajat as $o){
+            echo '<option value="'
+                . $o["ohjaaja_ID"] .'"'
+                .($o["ohjaaja_ID"] == $selectedID ? ' selected' : ''). '>' 
+                . $o["ohjaaja_ID"]
+                . $o["ohjaaja_nimi"]
+                .'</option>';
+        }
+        echo "</select><br/>";
+    }
 ?>
 
     <form action="add_movie.php" method="post">
         <label for="elokuvanimi">Elokuvan nimi:</label><br>
         <input type="text" name="elokuvanimi" id="elokuvanimi"><br>
-        <label for="genreID">Genre ID:</label><br>
+        <label for="genreID">Genre ID: <p> 1 = Komedia | 2 = Kauhu |  3 = Toiminta |  4 = Draama  | 5 = Fantasia |  6 = Animaatio |  7 = Romantiikka  | 8 = Supersankari |  9 = Sci-fi</p></label><br>
         
-        <?php 
+     <!--    <?php 
        createGenreDropdown($selectedId);
       
-       ?>
-        <!-- <input type="text" name="genreID" id="genreID"><br> -->
+       ?> -->
+        <input type="text" name="genreID" id="genreID" placeholder="ei vittu"> <br>
+        
         <label for="ohjaajaID">Ohjaajan ID:</label><br>
-        <input type="text" name="ohjaajaID" id="ohjaajaID"><br>
+        <?php
+        ohjaajaDropDown($selectedID);
+        ?>
+        <!-- <input type="text" name="ohjaajaID" id="ohjaajaID"><br> -->
         <label for="julkaisuvuosi">Julkaisuvuosi:</label><br>
         <input type="text" name="julkaisuvuosi" id="julkaisuvuosi"><br>
         <input type="submit" class="btn btn-primary" value="Add movie">
